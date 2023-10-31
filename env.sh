cbe9c92c3c111b72994a034e2cbb650a9c981309
Fawang <fwcheung@outlook.com>
2023-10-31 19:21:18 +0800

    polish env.sh

--- env.sh
+++ env.sh
@@ -2,6 +2,6 @@
 export XS_PROJECT_ROOT=$(pwd)
 export NEMU_HOME=$(pwd)/NEMU
 export AM_HOME=$(pwd)/nexus-am
-export NOOP_HOME=$(pwd)/XiangShan
+export NOOP_HOME=$(pwd)/Nanhu-V3
 export SPIKE_HOME=$(pwd)/riscv-isa-sim
 export DIFF_HOME=$(pwd)/difftest
